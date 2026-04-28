c53e0c0897e99011487d46b1cafc4f37f0711a5c
Krutarth <krutarth1307@gmail.com>
2026-04-28 22:11:19 +0530

    Fillter function

--- 8DiceRoller.js
+++ 8DiceRoller.js
@@ -19,35 +19,3 @@ function rollDice(){
     DiceValue.textContent = DiceValues.join(", ");
     DiceImage.innerHTML = Images.join('');
 }
-
-// Callback method
-
-// function sum(callback , x, y){
-//     let result = x+y;
-//     callback(result);
-// }
-
-// function display(result){
-//     console.log(result);
-// }
-
-// sum(display , 1, 2);
-
-//Ofeach
-
-let fruits = ["apple", "banana", "coconuy", "mango"];
-
-fruits.forEach(capatalize);
-fruits.forEach(display);
-
-function display(element){
-    console.log(element);
-}
-
-function toUpperCase(element, index, array){
-    array[index] = element.toUpperCase();
-} 
-
-function capatalize(element, index, array){
-    array[index] = element.charAt(0).toUpperCase() + element.slice(1) ;
-}
