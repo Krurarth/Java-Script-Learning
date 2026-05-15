8cf4b1e05f2b003919f8d1122013e47f0c56825f
Krutarth <krutarth1307@gmail.com>
2026-05-15 11:07:17 +0530

    intro to oop

---
+++ 10oop.js
@@ -0,0 +1,35 @@
+//CLASSES
+
+// class oop {
+
+
+//     constructor(name , age){
+//         this.age = age;
+//         this.name = name;
+//     }
+
+//     display(){
+//         console.log(`You're name is ${this.name}`);
+//          console.log(`You're age is ${this.age}`);
+
+//     }
+// }
+
+// const oop1 = new oop("Krutarth" , 19);
+
+// oop1.display();
+
+//OBJECTS
+
+// const person1 ={
+//     name : "Krutarth",
+//     age : 19,
+//     isStudent : true,
+
+//     sayHello : () => console.log(`Hey there... I'm Krutarth.`) ,
+// }
+
+// person1.sayHello();
+// console.log(person1.name);
+// console.log(person1.age);
+// console.log(person1.isStudent);
