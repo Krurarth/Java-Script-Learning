01a9a7a8659327d8b4111244e0fe659bec652ac2
Krutarth <krutarth1307@gmail.com>
2026-05-16 22:23:25 +0530

    Sorting in objects of array.

--- 12Sort.js
+++ 12Sort.js
@@ -3,3 +3,12 @@
 // numbers.sort((a, b) => a-b);
 
 // console.log(numbers);
+
+let people = [{name : "Krutarth" ,age : 19, gpa : 8.04 },
+              {name : "Diya" ,age : 19, gpa : 9.14 },
+              {name : "Tanay" ,age : 20, gpa : 7.26 }
+]
+
+people.sort( (a,b)=> b.gpa - a.gpa);
+
+console.log(people);
