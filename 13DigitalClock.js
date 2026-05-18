9bc67e08344708ac923f50bb88ce27011d3de7dc
Krutarth <krutarth1307@gmail.com>
2026-05-19 02:47:47 +0530

    ....

--- 13DigitalClock.js
+++ 13DigitalClock.js
@@ -1,3 +1,17 @@
-function count{
-    
+function count(){
+    const time = new Date();
+
+    let hour = time.getHours();
+    const AP = hour >= 12 ? "PM" :"AM" ;
+    hour = hour % 12 || 12 ;
+    hour = hour.toString().padStart(2,"0");
+    const minutes = time.getMinutes().toString().padStart(2,"0");
+    const second = time.getSeconds().toString().padStart(2,"0");
+
+    const Show = `${hour}:${minutes}:${second} ${AP}`;
+    document.getElementById("Clock").textContent = Show;
+  
 }
+
+count();
+setInterval(count,1000);
