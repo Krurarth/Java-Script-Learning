c209e95fd8b0204292db0601fe08548a5384468b
Krutarth <krutarth1307@gmail.com>
2026-05-31 12:00:45 +0530

    ..

--- 18RockPeperScissor.js
+++ 18RockPeperScissor.js
@@ -1,1 +1,32 @@
-//Will do this tommorrow.
+
+const choices = ["stone" , "paper" , "scissor"];
+const playerChoice = document.getElementById("playerChoice");
+const computerChoice = document.getElementById("computerChoice");
+const resultDisplay = document.getElementById("result");
+
+function value(playerChoice){ 
+    const computerChoice = choices[Math.floor(Math.random()*3)];
+    let result="";
+
+    switch(computerChoice){
+        case 'stone' : 
+    }
+
+    if(playerChoice === computerChoice){
+        result = `IT'S A TIE !`
+    }
+    else {
+        switch(playerChoice){
+            case 'paper' : (computerChoice == 'scissor')? result = `You lose!` : result = `You win!` ;
+            break;
+
+            case 'scissor' : (computerChoice == 'stone')? result = `You lose!` : result = `You win!` ;
+            break;
+
+            case 'stone' : (computerChoice == 'paper') ? result = `You lose!` : result = `You win!` ;
+            break;
+        }
+    }
+
+    result.text-Content = result;
+}
